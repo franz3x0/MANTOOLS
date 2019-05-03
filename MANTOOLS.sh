19ae8ef258d5df3c96b84adf97e338ce87b1cc76
Mr.F124NZ <50201087+franz3x0@users.noreply.github.com>
2019-05-03 18:39:10 +0700

    Add files via upload

---
+++ MANTOOLS.sh
@@ -0,0 +1,333 @@
+#!/bin/bash
+#mantools
+#By:FRANZ
+
+
+sleep 1
+clear
+echo "===============================[   WELCOME   ]=================================="
+echo "================================================================================"
+echo -e "\033[31m ▄▄▄  ▄▄▄     ▄▄     ▄▄▄   ▄▄  ▄▄▄▄▄▄▄▄    ▄▄▄▄      ▄▄▄▄    ▄▄          ▄▄▄▄   "
+echo -e "\033[31m ███  ███    ████    ███   ██  ▀▀▀██▀▀▀   ██▀▀██    ██▀▀██   ██        ▄█▀▀▀▀█ \033[0m"
+echo -e "\033[33m ████████    ████    ██▀█  ██     ██     ██    ██  ██    ██  ██        ██▄      "
+echo " ██ ██ ██   ██  ██   ██ ██ ██     ██     ██    ██  ██    ██  ██         ▀████▄  "
+echo -e "\033[33m ██ ▀▀ ██   ██████   ██  █▄██     ██     ██    ██  ██    ██  ██             ▀██ \033[0m"
+echo -e "\033[32m ██    ██  ▄██  ██▄  ██   ███     ██      ██▄▄██    ██▄▄██   ██▄▄▄▄▄▄  █▄▄▄▄▄█▀ "
+echo -e "\033[32m ▀▀    ▀▀  ▀▀    ▀▀  ▀▀   ▀▀▀     ▀▀       ▀▀▀▀      ▀▀▀▀    ▀▀▀▀▀▀▀▀   ▀▀▀▀▀ \033[0m"
+echo "================================================================================"
+echo -e "============================[\033[32m ALL ABOUT MANTOOLS\033[0m ]=============================="
+
+
+
+echo -e "\033[33mAUTHOR     : Mr.FRANZ"
+echo "SUPPORT me : (fb) franz exo "
+echo "           : (IG) franz_3x0 "
+echo "Github     : https://github.com/franz3x0 "
+echo "Version    : 2.7"
+
+echo -e "\033[34m========================================="
+echo "| [01] - Masuk Server NASA (LINUX only) |"
+echo "| [02] - Animasi CMATRIX                |"
+echo "| [03] - Nonton Film StarWars           |"
+echo "| [04] - Percepat Koneksi Internet      |"
+echo "| [05] - DDOS Attack With Trojan32Win   |"
+echo "| [06] - EXIT                           |"
+echo -e "\033[34m=========================================\033[0m"
+read -p "pilih nomor [ 1-6 ] :" bro
+
+if [ $bro = 1 ] || [ $bro = 1 ]
+then
+sleep 1
+sleep 1
+   echo   "# initializing ...."
+sleep 60
+clear
+echo -e "\033[31mANDA TIDAK MEMAKAI OS LINUX !!\033[0m"
+sleep 1
+sleep 1
+echo "exiting ..."
+sleep 1
+fi
+
+if
+ [ $bro = 2 ] || [ $bro = 2 ]
+then
+clear
+cmatrix
+fi
+
+if [ $bro = 3 ] || [ $bro = 3 ]
+then
+telnet towel.blinkenlights.nl
+fi
+
+if [ $bro = 4 ] || [ $bro = 4 ]
+then
+clear
+sleep 1
+echo -e "\033[32m==================\033[0m[\033[33m percepat koneksi internet\033[0m ]\033[32m=================="
+echo -e "\033[32m=========================[\033[37m By : FRANZ\033[0m \033[32m]=========================\033[0m"
+sleep 1
+echo -e "\033[37mpress ctrl+c to stop\033[32m "
+sleep 1
+echo ""
+echo ""
+ping 8.8.8.8
+fi
+
+if [ $bro = 5 ] || [ $bro = 5 ]
+then
+clear
+echo "======================================="
+echo -e "|||\033[31mFlooding Tcp Server's By:Mr.FRANZ\033[0m|||"
+echo "======================================="
+sleep 1
+echo "======( masukkan target Ip,Port,dan Jumlah paket . contoh : 983.443.65.8/80/135 )======"
+sleep 2
+sleep 3
+sleep 5
+sleep 6
+echo -e "\033[32m.... mengirim .... paket 1 .... Trojan32win"
+sleep 1
+echo ".... mengirim .... paket 2 .... Trojan32Win"
+sleep 1
+echo ".... mengirim .... paket 3 .... andromeda "
+sleep 1
+echo ".... mengirim .... paket 4 .... tcp flooding"
+sleep 1
+echo ".... mengirim .... paket 5 .... Trojan32Win"
+sleep 1
+echo ".... mengirim .... paket 6 .... andromeda"
+sleep 1
+echo ".... mengirim .... paket 7 .... tcp flooding"
+sleep 1
+echo ".... mengirim .... paket 8 .... Trojan32Win"
+sleep 1
+echo ".... mengirim .... paket 9 .... andromeda "
+sleep 1
+echo ".... mengirim .... paket 10 .... tcp flooding"
+sleep 1
+echo ".... mengirim .... paket 11 .... Trojan32Win"
+sleep 1
+echo ".... mengirim .... paket 12 .... andromeda"
+sleep 1
+echo ".... mengirim .... paket 13 .... tcp flooding"
+sleep 1
+echo ".... mengirim .... paket 14 .... Trojan32Win"
+sleep 1
+echo ".... mengirim .... paket 15 .... andromeda "
+sleep 1
+echo ".... mengirim .... paket 16 .... tcp flooding"
+sleep 1
+echo ".... mengirim .... paket 17 .... Trojan32Win"
+sleep 1
+echo ".... mengirim .... paket 18 .... andromeda"
+sleep 1
+echo ".... mengirim .... paket 19 .... tcp flooding"
+sleep 1
+echo ".... mengirim .... paket 20 .... Trojan32Win"
+sleep 1
+echo ".... mengirim .... paket 21 .... andromeda "
+sleep 1
+echo ".... mengirim .... paket 22 .... tcp flooding"
+sleep 1
+echo ".... mengirim .... paket 23 .... Trojan32Win"
+sleep 1
+echo ".... mengirim .... paket 24 .... andromeda"
+sleep 1
+echo ".... mengirim .... paket 25 .... tcp flooding"
+sleep 1
+echo ".... mengirim .... paket 26 .... Trojan32Win"
+sleep 1
+echo ".... mengirim .... paket 27 .... andromeda "
+sleep 1
+echo ".... mengirim .... paket 28 .... tcp flooding"
+sleep 1
+echo ".... mengirim .... paket 29 .... Trojan32Win"
+sleep 1
+echo ".... mengirim .... paket 30 .... andromeda"
+sleep 1
+echo ".... mengirim .... paket 31 .... tcp flooding"
+sleep 1
+echo ".... mengirim .... paket 32 .... Trojan32Win"
+sleep 1
+echo ".... mengirim .... paket 33 .... andromeda "
+sleep 1
+echo ".... mengirim .... paket 34 .... tcp flooding"
+sleep 1
+echo ".... mengirim .... paket 35 .... Trojan32Win"
+sleep 1
+echo ".... mengirim .... paket 36 .... andromeda"
+sleep 1
+echo ".... mengirim .... paket 37 .... tcp flooding"
+sleep 1
+echo ".... mengirim .... paket 38 .... Trojan32Win"
+sleep 1
+echo ".... mengirim .... paket 39 .... andromeda "
+sleep 1
+echo ".... mengirim .... paket 40 .... tcp flooding"
+sleep 1
+echo ".... mengirim .... paket 41 .... Trojan32Win"
+sleep 1
+echo ".... mengirim .... paket 42 .... andromeda"
+sleep 1
+echo ".... mengirim .... paket 43 .... tcp flooding"
+sleep 1
+echo ".... mengirim .... paket 44 .... Trojan32Win"
+sleep 1
+echo ".... mengirim .... paket 45 .... andromeda "
+sleep 1
+echo ".... mengirim .... paket 46 .... tcp flooding"
+sleep 1
+echo ".... mengirim .... paket 47 .... Trojan32Win"
+sleep 1
+echo ".... mengirim .... paket 48 .... andromeda"
+sleep 1
+echo ".... mengirim .... paket 49 .... tcp flooding"
+sleep 1
+echo ".... mengirim .... paket 50 .... Trojan32Win"
+sleep 1
+echo ".... mengirim .... paket 51 .... andromeda "
+sleep 1
+echo ".... mengirim .... paket 52 .... tcp flooding"
+sleep 1
+echo ".... mengirim .... paket 53 .... Trojan32Win"
+sleep 1
+echo ".... mengirim .... paket 54 .... andromeda"
+sleep 1
+echo ".... mengirim .... paket 55 .... tcp flooding"
+sleep 1
+echo ".... mengirim .... paket 56 .... Trojan32Win"
+sleep 1
+echo ".... mengirim .... paket 57 .... andromeda "
+sleep 1
+echo ".... mengirim .... paket 58 .... tcp flooding"
+sleep 1
+echo ".... mengirim .... paket 59 .... Trojan32Win"
+sleep 1
+echo ".... mengirim .... paket 60 .... andromeda"
+sleep 1
+echo ".... mengirim .... paket 61 .... tcp flooding"
+sleep 1
+echo ".... mengirim .... paket 62 .... Trojan32Win"
+sleep 1
+echo ".... mengirim .... paket 63 .... andromeda "
+sleep 1
+echo ".... mengirim .... paket 64 .... tcp flooding"
+sleep 1
+echo ".... mengirim .... paket 65 .... Trojan32Win"
+sleep 1
+echo ".... mengirim .... paket 67 .... andromeda"
+sleep 1
+echo ".... mengirim .... paket 68 .... tcp flooding"
+sleep 1
+echo ".... mengirim .... paket 69 .... Trojan32Win"
+sleep 1
+echo ".... mengirim .... paket 70 .... andromeda "
+sleep 1
+echo ".... mengirim .... paket 71 .... tcp flooding"
+sleep 1
+echo ".... mengirim .... paket 72 .... Trojan32Win"
+sleep 1
+echo ".... mengirim .... paket 73 .... andromeda"
+sleep 1
+echo ".... mengirim .... paket 75 .... tcp flooding"
+sleep 1
+echo ".... mengirim .... paket 76 .... Trojan32Win"
+sleep 1
+echo -e "\033[31mServer Down ..."
+sleep 1
+echo "Server Down ..."
+sleep 1
+echo "Server Down ..."
+sleep 1
+echo "Server Down ..."
+sleep 1
+echo -e "\033[31mServer Down ...\033[0m"
+sleep 1
+echo -e "\033[32m.... mengirim .... paket 77 .... andromeda "
+sleep 1
+echo ".... mengirim .... paket 78 .... tcp flooding"
+sleep 1
+echo ".... mengirim .... paket 79 .... Trojan32Win"
+sleep 1
+echo ".... mengirim .... paket 80 .... andromeda"
+sleep 1
+echo ".... mengirim .... paket 81 .... tcp flooding"
+sleep 1
+echo ".... mengirim .... paket 82 .... Trojan32Win"
+sleep 1
+echo ".... mengirim .... paket 83 .... andromeda "
+sleep 1
+echo ".... mengirim .... paket 84 .... tcp flooding"
+sleep 1
+echo ".... mengirim .... paket 85 .... Trojan32Win"
+sleep 1
+echo ".... mengirim .... paket 86 .... andromeda"
+sleep 1
+echo ".... mengirim .... paket 87 .... tcp flooding"
+sleep 1
+echo ".... mengirim .... paket 88 .... Trojan32Win"
+sleep 1
+echo ".... mengirim .... paket 89 .... andromeda "
+sleep 1
+echo ".... mengirim .... paket 90 .... tcp flooding"
+sleep 1
+echo ".... mengirim .... paket 91 .... Trojan32Win"
+sleep 1
+echo ".... mengirim .... paket 92 .... andromeda"
+sleep 1
+echo ".... mengirim .... paket 93 .... tcp flooding"
+sleep 1
+echo ".... mengirim .... paket 94 .... andromeda"
+sleep 1
+echo ".... mengirim .... paket 95 .... tcp flooding"
+sleep 1
+echo ".... mengirim .... paket 96 .... Trojan32Win"
+sleep 1
+echo ".... mengirim .... paket 97 .... andromeda "
+sleep 1
+echo ".... mengirim .... paket 98 .... tcp flooding"
+sleep 1
+echo ".... mengirim .... paket 99 .... Trojan32Win"
+sleep 1
+echo ".... mengirim .... paket 100 .... andromeda"
+sleep 1
+echo ".... mengirim .... paket 101 .... tcp flooding"
+sleep 1
+echo ".... mengirim .... paket 102 .... Trojan32Win"
+sleep 1
+echo ".... mengirim .... paket 103 .... andromeda"
+sleep 1
+echo ".... mengirim .... paket 104 .... tcp flooding"
+sleep 1
+echo ".... mengirim .... paket 105 .... Trojan32Win"
+sleep 1
+echo ".... mengirim .... paket 106 .... andromeda "
+echo -e "\033[31mServer Down ..."
+sleep 1
+echo "Server Down ..."
+sleep 1
+echo "Server Down ..."
+sleep 1
+echo "Server Down ..."
+sleep 1
+echo "Server Down ..."
+sleep 1
+echo "Server Down ..."
+sleep 5
+echo "No Connection To Server "
+sleep 4
+echo -e "\033[37m EXITING >>>>>>"
+sleep 3
+fi
+
+if [ $bro = 6 ] || [ $bro = 6 ]
+then
+sleep 1
+echo " Terima kasih Sudah Menggunkan Tools Ini "
+sleep 1
+echo " Support Terus Untuk Tools Terbaru Lainnya "
+sleep 1
+fi
+
